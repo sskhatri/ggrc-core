cd1f08113b868962b620f44af154df7b7e0a4d13
Saroj Silwal Khatri <skhatri@skhatri.svl.corp.google.com>
2017-08-17 10:47:23 -0700

    Testing db migration

---
+++ src/ggrc/maintenance.py
@@ -0,0 +1,39 @@
+# Copyright (C) 2017 Google Inc.
+# Licensed under http://www.apache.org/licenses/LICENSE-2.0 <see LICENSE file>
+
+"""Sets up Flask app."""
+
+from collections import Iterable
+
+import re
+from logging import getLogger
+# from logging.config import dictConfig as setup_logging
+
+from flask import Flask
+# from flask.ext.sqlalchemy import get_debug_queries
+# from flask.ext.sqlalchemy import SQLAlchemy
+# from tabulate import tabulate
+# from sqlalchemy import event
+
+# from ggrc import db
+# from ggrc import extensions
+# from ggrc import notifications
+from ggrc import settings
+
+
+# setup_logging(settings.LOGGING)
+
+# pylint: disable=invalid-name
+logger = getLogger(__name__)
+
+logger.info('HEHE inside maintenance.py')
+
+app = Flask('ggrc_test', instance_relative_config=True)  # noqa: valid constant name
+app.config.from_object(settings)
+if "public_config" not in app.config:
+  app.config.public_config = {}
+
+for key in settings.exports:
+  app.config.public_config[key] = app.config[key]
+
+from ggrc.maintenance_views import test
