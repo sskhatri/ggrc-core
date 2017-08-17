7b0ad62a342ad3f6236815d6db4640d9a406cefb
Saroj Silwal Khatri <skhatri@skhatri.svl.corp.google.com>
2017-08-17 10:47:23 -0700

    Testing db migration

---
+++ src/ggrc/maintenance_views/test.py
@@ -0,0 +1,18 @@
+from ggrc.maintenance import app
+from ggrc import migrate
+import sys
+import os
+
+@app.route('/maintenance/test')
+def index():
+  #logging.info('HEHE DEV_PREFIX : {}'.format(os.environ['DEV_PREFIX']))
+  # dev_prefix = os.environ['DEV_PREFIX']
+  # old_module_paths = [
+  #   dev_prefix + '/opt/google-cloud-sdk/platform/google_appengine/lib/google-api-python-client',
+  #   dev_prefix + '/opt/google-cloud-sdk/platform/google_appengine/lib/python-gflags',
+  #   dev_prefix + '/opt/google-cloud-sdk/platform/google_appengine/lib/httplib2',
+  # ]
+  # for old_path in old_module_paths:
+  #   sys.path.insert(0, old_path)
+  migrate.upgradeall()
+  return "HEHE migration successful"
